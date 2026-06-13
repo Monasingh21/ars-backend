d5552124c69a851405ffc22e358f025b287f3b33
Monasingh21 <monasingh.k.21@gmail.com>
2026-06-13 13:01:09 +0530

    Add a entity class

---
+++ src/main/java/com/codegnan/app/arsbackend/entiry/User.java
@@ -0,0 +1,79 @@
+package com.codegnan.app.arsbackend.entiry;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private int userId;
+	@Column(name = "full_name")
+	@NotBlank(message = "Full Name cannot be left blank")
+	private String fullName;
+	@Column(name = "email")
+	@NotBlank(message = "Email cannot be left blank")
+	private String email;
+	@Column(name = "password")
+	@NotBlank(message = "Password cannot be left blank")
+	private String password;
+	@Column(name = "role")
+	@NotBlank(message = "Role cannot be left blank")
+	private String role;
+
+	public User() {
+	}
+
+	public User(int userId, String fullName, String email, String password, String role) {
+		this.userId = userId;
+		this.fullName = fullName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getEUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", fullName=" + fullName + ", email=" + email + ", password=" + password + ", role=" + role + "]";
+	}
+
+}
