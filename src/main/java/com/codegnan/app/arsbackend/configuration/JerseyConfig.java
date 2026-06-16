b35077733944b203db2317d38742bbf5289e6ffd
Monasingh21 <monasingh.k.21@gmail.com>
2026-06-16 14:21:55 +0530

    Add sign up process with corsfilter

--- src/main/java/com/codegnan/app/arsbackend/configuration/JerseyConfig.java
+++ src/main/java/com/codegnan/app/arsbackend/configuration/JerseyConfig.java
@@ -1,11 +1,14 @@
-package com.codegnan.app.arsbackend.resource;
+package com.codegnan.app.arsbackend.configuration;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
+
+import com.codegnan.app.arsbackend.resource.UserResourceImpl;
 
 @Configuration
 public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
 		register(UserResourceImpl.class);
+		register(CorsFilter.class);
 	}
 }
